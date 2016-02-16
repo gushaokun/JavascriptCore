fc9be0a1fbe18290944c61396886f9d016db4d71
gushaokun <gushaokun@gmail.com>
2016-02-16 23:38:02 +0800

    Initial Commit

---
+++ JavascriptCore/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JavascriptCore
+//
+//  Created by Gavin on 16/2/16.
+//  Copyright © 2016年 Gavin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
